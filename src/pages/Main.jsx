023b5aec2dd0707726df705689d35d47ccdb3c6b
Rene Cruz <renecruz.0711@gmail.com>
2023-10-23 15:43:25 -0600

    Adding application main pages

---
+++ src/pages/Main.jsx
@@ -0,0 +1,15 @@
+
+import FilterNavbar from "../components/FilterNavbar";
+
+const Main = () => {
+
+  return (
+    <div>
+        <FilterNavbar />
+      Hi, I am Rene
+
+    </div>
+  );
+};
+
+export default Main;
